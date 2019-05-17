02479a0bdff30b89b1b1c1d3f47aa5811f7bedba
Muhammad Ihsan Ananta <mihsanananta2@live.com>
2019-05-18 05:22:25 +0900

    Update package.json

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -3,14 +3,22 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => (
-  <div className="rmdb-header">
-    <div className="rmdb-header-content">
-      <Link to="/">
-        <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+  <div className='rmdb-header'>
+    <div className='rmdb-header-content'>
+      <Link to='/'>
+        <img
+          className='rmdb-logo'
+          src='./images/reactMovie_logo.png'
+          alt='rmdb-logo'
+        />
       </Link>
-      <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
+      <img
+        className='rmdb-tmdb-logo'
+        src='./images/tmdb_logo.png'
+        alt='tmdb-logo'
+      />
     </div>
   </div>
-)
+);
 
 export default Header;
